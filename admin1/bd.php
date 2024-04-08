ebb0f7af25428a6354035b44ce2dea8d352524c5
Andrey Abalakov <a.s.abalakov@yandex.ru>
2024-04-08 17:50:49 +0300

    Мелкие правки

---
+++ admin1/bd.php
@@ -0,0 +1,16 @@
+<?php
+
+if(empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == "off"){
+    $redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    header('HTTP/1.1 301 Moved Permanently');
+    header('Location: ' . $redirect);
+    exit();
+}
+
+require_once(str_replace('\\\\','/',dirname(__FILE__)).'/configuration.php');
+
+mysql_connect($GLOBALS['config']['bd_hostname'], $GLOBALS['config']['bd_username'], $GLOBALS['config']['bd_password']) or die("Не могу подключиться к БД!");
+mysql_select_db($GLOBALS['config']['bd_basename']) or die("БД отсутствует!");
+mysql_set_charset('utf8mb4');
+
+?>
