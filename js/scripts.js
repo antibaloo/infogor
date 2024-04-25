832462deb5d19a45db87fd02ffa28129acc2a1bf
Andrey Abalakov <a.s.abalakov@yandex.ru>
2024-04-25 16:27:00 +0300

    Мелкие правки

--- js/scripts.js
+++ js/scripts.js
@@ -20,40 +20,6 @@ $(".slResponsive").slick({
   ]
 });
 
-$(".hero-slides").slick({
-  arrows: true,
-  infinite: true,
-  centerMode: true,
-  centerPadding: "20%",
-  autoplay: true,
-  autoplaySpeed: 5000,
-  prevArrow: "<img src='/img/slide-arrow-left.png' class='slick-prev' alt=''>",
-  nextArrow: "<img src='/img/slide-arrow-right.png' class='slick-next' alt=''>",
-  appendArrows: $(".hero .slider-nav"),
-  responsive: [
-     {
-      breakpoint: 1681, //1441+1
-      settings:{
-        centerPadding: "20%",
-        centerMode: true,
-      }
-    },
-    {
-      breakpoint: 1441, //1441+1
-      settings:{
-        centerPadding: "17%",
-        centerMode: true,
-      }
-    },    
-    {
-      breakpoint: 1201, //1200+1
-      settings:{
-        centerMode: false,
-      }
-    },
-  ],
-});
-
 $(document).ready(function () {
   // $(".dropdown").hover(function () {
   //   console.log($(this).find(".dropdown-toggle"));
